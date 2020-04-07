23da7581e3570ad4aed97aff97f21ecc6ede6c84
Ian S. McBride <ian.s.mcb@gmail.com>
2020-04-07 16:33:17 -0400

    Add 3 fields to User model

--- models/user.js
+++ models/user.js
@@ -1,7 +1,22 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    email: DataTypes.STRING
+    email: {
+      type: DataTypes.STRING(32),
+      validate: { isEmail: true }
+    },
+    firstName: {
+      type: DataTypes.STRING(32),
+      validate: { isNull: false }
+    },
+    lastName: {
+      type: DataTypes.STRING(32),
+      validate: { isNull: false }
+    },
+    password: {
+      type: DataTypes.STRING(),
+      validate: { isNull: false, len: [8, 64] }
+    }
   }, {});
   User.associate = function(models) {
     // associations can be defined here
