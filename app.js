22e28863f48086f24fda7ae0fb423644b1ff1511
Ian S. McBride <ian.s.mcb@gmail.com>
2020-04-11 21:17:09 -0400

    Remove more Jade code from app.js

--- app.js
+++ app.js
@@ -30,8 +30,8 @@ app.use(function(err, req, res, next) {
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
   // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  console.log(err)
+  res.sendStatus(err.status || 500);
 });
 
 module.exports = app;
